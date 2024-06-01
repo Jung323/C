c603d2a60839efb8dec15303f96a1f7a317c81c6
Jung323 <102010668+Jung323@users.noreply.github.com>
2024-06-01 18:06:10 +0900

    Add files via upload

--- day13_240511.c
+++ day13_240511.c
@@ -43,6 +43,50 @@
 
 
 //ex) 포인터의 값을 바꾸는 함수의 매개변수
+//#include <stdio.h>
+//
+//void swap_ptr(char** ppa, char** ppb); //함수 원형 선언
+//
+//int main(void)
+//{
+//	char* pa = "success";
+//	char* pb = "failure";
+//	
+//	printf("pa: %s, pb: %s\n", pa, pb); //바꾸기 전에 문자열 출력
+//	swap_ptr(&pa, &pb); //함수 호출
+//
+//	printf("pa: %s, pb: %s\n", pa, pb); //바꾼 후에 문자열 출력
+//
+//	return 0;
+//}
+//
+//void swap_ptr(char** ppa, char** ppb)
+//{
+//	char* pt; //교환을 위한 임시변수
+//
+//	pt = *ppa;
+//	*ppa = *ppb;
+//	*ppb = pt;
+//}
+
+
+//ex) 포인터 배열을 매개변수로 받는 함수
 #include <stdio.h>
 
-void swap_ptr()
+void print_ptr(char** pps, int cnt); //함수 원형 선언
+
+int main(void)
+{
+	char* ptr_ary[] = { "eagle","tiger","lion","squirrel" }; //초기화
+
+	print_ptr(ptr_ary, 4); //배열명과 배열요소수를 넘겨주고 호출
+
+	return 0;
+}
+
+void print_ptr(char** pps, int cnt)
+{
+	int i;
+	for (i = 0; i < cnt; i++)
+		printf("%s\n", pps[i]); //이중포인터를 배열명처럼 사용
+}
